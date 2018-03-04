8da6329dd45cee633fffda6d2af391e0604c8cd3
HomosassaCoder <36938050+HomosassaCoder@users.noreply.github.com>
2018-03-04 14:51:25 -0500

    Add files via upload

---
+++ AJPHello.java
@@ -0,0 +1,12 @@
+public class AJPHello  { 
+    
+   public static void main(String[] args) {
+        // generate some simple output
+        System.out.println("Hello, World!");
+        System.out.println("Gosh Darned!");   // Silly me
+        System.out.println("I'll be damned!");   // new stuff
+        System.out.println("I am a genius!");
+        System.out.println("Who dare to argue?");
+        System.out.println("Now fetch me my hosenfefer!");
+    }
+}
